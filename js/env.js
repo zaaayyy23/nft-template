a20e7805970fe443ce5b70105e053d9bee3eaf40
zaaayyy23 <153271214+zaaayyy23@users.noreply.github.com>
2024-03-05 00:56:50 +0000

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,5 @@
-const contractAddress = ""; // Paste the address of the NFT collection.
+const contractAddress = "0x7570A85A239Ca2b80b953f07aE707A9dE02C7e90"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
-const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
+// const chainId = "0x507"; // Moonbase
+const chainId = "0x504"; // Moonbeam
 // const chainId = "0x250"; // Astar
